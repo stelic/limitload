d46e895040e11b7809797c609c186336255176d4
Nikola Milentijevic <nikolmiv@gmail.com>
2015-08-03 00:36:05 +0200

    Extract 2d grid interpolation to tables
    The UnitGrid2 class can be initialized with a regular unit grid
    in form of list of lists of values at grid nodes, list of arrays,
    PNM image, path to PNM image, or scalar value.

--- src/core/table.py
+++ src/core/table.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 
+from array import array
 from bisect import bisect
+
+from pandac.PandaModules import PNMImage, Point2D
 
 
 class Table1 (object):
@@ -123,5 +126,206 @@ def _get_interp_indices (seq, val):
     elif ir >= len(seq):
         il -= 1; ir -= 1
     return il, ir
+
+
+class UnitGrid2 (object):
+
+    def __init__ (self, grid):
+
+        if isinstance(grid, list):
+            if isinstance(grid[0], array):
+                pass
+            else:
+                grid = [array("d", grow) for grow in grid]
+        elif isinstance(grid, basestring):
+            grid = UnitGrid2._grid_from_pnm(PNMImage(grid))
+        elif isinstance(grid, PNMImage):
+            grid = UnitGrid2._grid_from_pnm(grid)
+        elif isinstance(grid, (float, int)):
+            grid = [array("d", [float(grid)])]
+        else:
+            raise StandardError("Unknown kind of grid.")
+
+        self._grid = grid
+        self._num_x = len(grid)
+        self._num_y = len(grid[0])
+
+
+    @staticmethod
+    def _grid_from_pnm (pnm):
+
+        sx = pnm.getReadXSize()
+        sy = pnm.getReadYSize()
+        grid = [array("d", [0.0] * sy) for i in xrange(sx)]
+        for i in xrange(sx):
+            for j in xrange(sy):
+                grid[i][j] = pnm.getGray(i, sy - j - 1)
+        return grid
+
+
+    def num_x (self):
+
+        return self._num_x
+
+
+    def num_y (self):
+
+        return self._num_y
+
+
+    def __call__ (self, xr, yr, tol=0.0, periodic=False):
+
+        return UnitGrid2._interpolate_unit(
+            self._grid, self._num_x, self._num_y, xr, yr, tol, periodic)
+
+
+    @staticmethod
+    def _interpolate_unit (grid, numx, numy, xr, yr,
+                           tol=0.0, periodic=False):
+
+        cls = UnitGrid2
+        Pt = Point2D
+        #print "--interpolate-unit-05", "=========="
+
+        #print ("--interpolate-unit-10  "
+               #"xr=%.6f  yr=%.6f  numx=%d  numy=%d" % (xr, yr, numx, numy))
+
+        ret = cls._unit_to_index2(numx, numy, xr, yr, tol, periodic)
+        if not ret:
+            raise StandardError("Point out of range.")
+        i1, j1, xr, yr = ret
+        #print ("--interpolate-unit-12  "
+               #"xr=%.6f  yr=%.6f" % (xr, yr))
+        x1 = (i1 + 0.5) / numx
+        y1 = (j1 + 0.5) / numy
+        di = 1 if xr > x1 else -1
+        dj = 1 if yr > y1 else -1
+        di2 = di; dj2 = 0
+        i2, j2 = cls._shift_index2(numx, numy, i1, j1, di2, dj2, periodic)
+        di3 = di; dj3 = dj
+        i3, j3 = cls._shift_index2(numx, numy, i1, j1, di3, dj3, periodic)
+        di4 = 0; dj4 = dj
+        i4, j4 = cls._shift_index2(numx, numy, i1, j1, di4, dj4, periodic)
+        # NOTE: Must not use iN, jN below when computing relative coordinates,
+        # in case there was wrap due to periodicity.
+        #print ("--interpolate-unit-20  "
+               #"ij1=%s  ij2=%s  ij3=%s  ij4=%s" % ((i1, j1), (i2, j2), (i3, j3), (i4, j4)))
+
+        if i2 >= 0 and i3 >= 0 and i4 >= 0: # three neighbors
+            # Bilinear interpolation.
+            x3 = (i1 + di3 + 0.5) / numx
+            y3 = (j1 + dj3 + 0.5) / numy
+            fd = (x3 - x1) * (y3 - y1)
+            f1 = (x3 - xr) * (y3 - yr)
+            f2 = (xr - x1) * (y3 - yr)
+            f3 = (xr - x1) * (yr - y1)
+            f4 = (x3 - xr) * (yr - y1)
+            v1 = grid[i1][j1]
+            v2 = grid[i2][j2]
+            v3 = grid[i3][j3]
+            v4 = grid[i4][j4]
+            v = (v1 * f1 + v2 * f2 + v3 * f3 + v4 * f4) / fd
+            #print ("--interpolate-unit-30  "
+                   #"x1=%.6f  y1=%.6f  x3=%.6f  y3=%.6f" % (x1, y1, x3, y3))
+
+        elif i2 >= 0 or i3 >= 0 or i4 >= 0: # one neighbor
+            # Linear interpolation.
+            if i2 >= 0:
+                i2, j2, di2, dj2 = i2, j2, di2, dj2
+            elif i3 >= 0:
+                i2, j2, di2, dj2 = i3, j3, di3, dj3
+            elif i4 >= 0:
+                i2, j2, di2, dj2 = i4, j4, di4, dj4
+            r = Pt(xr, yr)
+            x2 = (i1 + di2 + 0.5) / numx
+            y2 = (j1 + dj2 + 0.5) / numy
+            r1 = Pt(x1, y1)
+            r2 = Pt(x2, y2)
+            udr = r2 - r1
+            udr.normalize()
+            l1 = (r - r1).dot(udr)
+            l2 = (r2 - r).dot(udr)
+            v1 = grid[i1][j1]
+            v2 = grid[i2][j2]
+            v = (v1 * l2 + v2 * l1) / (l1 + l2)
+            #print ("--interpolate-unit-32  "
+                   #"x1=%.6f  y1=%.6f  x2=%.6f  y2=%.6f" % (x1, y1, x2, y2))
+
+        else: # no neighbors (two neighbors not possible)
+            v = grid[i1][j1]
+            #print ("--interpolate-unit-34  "
+                   #"x1=%.6f  y1=%.6f" % (x1, y1))
+
+        return v
+
+
+    @staticmethod
+    def _unit_to_index (num, u, tol=0.0, periodic=False):
+
+        k = int(u * num)
+        if k < 0:
+            if periodic:
+                while k < 0:
+                    k += num
+                    u += 1.0
+            elif u > -tol / num:
+                k = 0
+                u = 0.0
+            else: # not valid
+                k = -1
+                u = -1.0
+        elif k >= num:
+            if periodic:
+                while k >= num:
+                    k -= num
+                    u -= 1.0
+            elif u < 1.0 + tol / num:
+                k = num - 1
+                u = 1.0
+            else: # not valid
+                k = -1
+                u = -1.0
+        return k, u
+
+
+    @staticmethod
+    def _unit_to_index2 (numx, numy, xr, yr, tol=0.0, periodic=False):
+
+        cls = UnitGrid2
+        i, xr = cls._unit_to_index(numx, xr, tol, periodic)
+        j, yr = cls._unit_to_index(numy, yr, tol, periodic)
+        if i < 0 or j < 0: # not valid
+            i = -1; j = -1
+        return i, j, xr, yr
+
+
+    @staticmethod
+    def _shift_index (num, k, dk, periodic=False):
+
+        k += dk
+        if k < 0:
+            if periodic:
+                while k < 0:
+                    k += num
+            else: # not valid
+                k = -1
+        elif k >= num:
+            if periodic:
+                while k >= num:
+                    k -= num
+            else: # not valid
+                k = -1
+        return k
+
+
+    @staticmethod
+    def _shift_index2 (numx, numy, i, j, di, dj, periodic=False):
+
+        cls = UnitGrid2
+        i = cls._shift_index(numx, i, di, periodic)
+        j = cls._shift_index(numy, j, dj, periodic)
+        if i < 0 or j < 0: # not valid
+            i = -1; j = -1
+        return i, j
 
 
